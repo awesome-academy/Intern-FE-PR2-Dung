1ed11afd2e2dc9320da711e5d542c750161f0cf1
nguyen quoc dung <nguyenquocdung281099@gmail.com>
2021-07-12 13:55:17 +0700

    footer header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import ProfilePage from "./pages/profilePage/index";
 import "./App.css";
 import React from "react";
+import RouterPublic from "./component/route/publicRoute";
 
 function App() {
   return (
     <div className="App">
-      <ProfilePage />
+      <RouterPublic component={ProfilePage} to="/" />
     </div>
   );
 }
